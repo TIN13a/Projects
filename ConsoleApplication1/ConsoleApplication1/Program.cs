7f70a5f689fdab934fb38a2e41e5e03e537531f0
unknown <lutzd@T4W16.edu.b.teko.ch>
2014-01-11 10:57:00 +0100

    console write line added (hello world)

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleApplication1 {
+    class Program {
+
+        static void Main(string[] args) {
+            System.Console.WriteLine("Hello World\n");
         }
+
     }
 }
