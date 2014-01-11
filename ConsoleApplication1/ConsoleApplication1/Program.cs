8df4219e2065630c4f56f2c29485db9e9f6cee83
larsanderegg <lars.anderegg@gmail.com>
2014-01-11 11:21:17 +0100

    add comment

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1 {
             System.Threading.Thread.Sleep(5000);
             System.Console.WriteLine("Hello World\n");
             System.Threading.Thread.Sleep(5000);
+            //asdasd
         }
 
     }
