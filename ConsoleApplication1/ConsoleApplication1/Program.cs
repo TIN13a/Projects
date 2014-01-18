af69915345a00c21b8591507fa67e5b2e9b137d1
deinonichus <ds.lutz@gmail.com>
2014-01-18 10:51:23 +0100

    new method calc

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,16 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1 {
-    class Program {
+    namespace ConsoleApplication1 {
+        class Program {
 
-        static void Main(string[] args) {
-            System.Console.WriteLine("Hello World\n");
-            System.Threading.Thread.Sleep(5000);
-            System.Console.WriteLine("Hello World\n");
-            System.Threading.Thread.Sleep(5000);
-            //asdasd
-            //gsesch das?
+            static void Main(string[] args) {
+                System.Console.WriteLine("Hello World\n");
+                System.Threading.Thread.Sleep(5000);
+                System.Console.WriteLine("Hello World\n");
+                System.Threading.Thread.Sleep(5000);
+
+                for (int i = 50; i < 71; i++) {
+                    string test = String.Empty;
+                }
+            }
+
+            public static int calculate(byte argument) {
+                return 0;
+            }
         }
-
     }
 }
