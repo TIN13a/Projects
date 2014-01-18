309e54b7e881cb985c2ad4b4a98e73af76a8717d
deinonichus <ds.lutz@gmail.com>
2014-01-18 11:31:23 +0100

    created

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("The program picked a random number between 0 and 100. Guess which one.");
+            //byte randomNumber = 
+
+        }
+    }
+}
