856ecdb3684176d9b92b6c02e95bd8ea6ea00c3f
deinonichus <ds.lutz@gmail.com>
2014-01-21 21:49:27 +0100

    solved, except for an uncatched use case (entering single letters)

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -7,7 +7,34 @@ using System.Threading.Tasks;
 namespace GuessingGame {
     class Program {
         static void Main(string[] args) {
-            // Test
+            Console.WriteLine("The program picked a random number between 0 and 100. Guess which one.");
+            Random random = new Random();
+            int rndDigit = random.Next(0, 101);
+            int guessDigit; 
+            int counter = 0;
+            while (true) {
+                try {
+                    guessDigit = Convert.ToInt32(Console.ReadLine(), 16);
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Type in a number you fucking cuntface!");
+                    continue;
+                }
+                
+                if (guessDigit == rndDigit) {
+                    break;
+                }
+                else if (guessDigit < rndDigit) {
+                    Console.WriteLine("the random number is bigger.");
+                }
+                else if (guessDigit > rndDigit) {
+                    Console.WriteLine("the random number is smaller.");
+                }
+                Console.WriteLine("Wrong.");
+                counter++;
+            }
+            Console.WriteLine("Your last guess was correct. The random number was: " + rndDigit + "\nYou guessed" + counter + "times.");
+            Console.ReadKey();
         }
     }
 }
