15f4b6cbe3341b7bb6bfde7ac7933ecf2748ed81
deinonichus <ds.lutz@gmail.com>
2014-01-21 21:24:41 +0100

    created project

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -7,9 +7,7 @@ using System.Threading.Tasks;
 namespace GuessingGame {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("The program picked a random number between 0 and 100. Guess which one.");
-            //byte randomNumber = 
-
+            // Test
         }
     }
 }
