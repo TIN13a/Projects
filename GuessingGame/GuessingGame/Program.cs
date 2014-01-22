1fe7d29e21dde30b67ed4a557839bb77e51e95e7
deinonichus <ds.lutz@gmail.com>
2014-01-22 21:11:09 +0100

    improved string to int converting

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -10,31 +10,16 @@ namespace GuessingGame {
             Console.WriteLine("The program picked a random number between 0 and 100. Guess which one.");
             Random random = new Random();
             int rndDigit = random.Next(0, 101);
-            int guessDigit; 
-            int counter = 0;
-
-            //while (true) { 
-            //    Console.Write("testA => ");
-            //    int testA = Convert.ToInt32(Console.ReadLine(), 16);
-            //    Console.Write("testB => ");
-            //    int testB = Convert.ToInt32(Console.ReadLine(), 16);
-
-            //    if (testA == testB) {
-            //        Console.WriteLine("equal");
-            //    }
-            //    else if (testA < testB) {
-            //        Console.WriteLine("A is smaller than B");
-            //    }
-            //    else if (testA > testB) {
-            //        Console.WriteLine("A is bigger than B");
-            //    }
-            //}
+            int guessDigit;
+            int counter = 1;
             
             while (true) {
                 try {
                     // 2014-01-21 DSL
                     // If the user enters a single letter (e.g. "a") it counts as number. 
-                    guessDigit = Convert.ToInt32(Console.ReadLine(), 16);
+                    //guessDigit = Convert.ToInt32(Console.ReadLine(), 16);
+                    guessDigit = Int32.Parse(Console.ReadLine());
+
                 }
                 catch (FormatException) {
                     Console.WriteLine("Type in a number you fucking cuntface!");
@@ -51,7 +36,6 @@ namespace GuessingGame {
                 else if (guessDigit > rndDigit) {
                     Console.WriteLine("the random number is smaller.");
                 }
-                Console.WriteLine("Wrong.");
                 counter++;
             }
             Console.ReadKey();
