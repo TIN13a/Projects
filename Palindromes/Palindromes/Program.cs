2c7e3e02c81433477aedd98f5b08005d825310cd
deinonichus <ds.lutz@gmail.com>
2014-01-23 22:13:26 +0100

    conversion from char array to string wasn't correct

---
+++ Palindromes/Palindromes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                Console.WriteLine("Enter a word/phrase to see if it's a palindrome.");
+                string input = Console.ReadLine().ToString();
+                Console.WriteLine();
+                char[] arrFwd = input.ToCharArray();
+                char[] arrBwd = new char[arrFwd.Length];
+                Array.Copy(arrFwd, arrBwd, arrFwd.Length);
+                Array.Reverse(arrBwd);
+                string stringFwd = new string(arrFwd);
+                string stringBwd = new string(arrBwd);
+                if (stringFwd == stringBwd) {
+                    Console.WriteLine("palindrome:\tyes");
+                }
+                else {
+                    Console.WriteLine("palindrome:\tno");
+                }
+                Console.WriteLine("forward:\t" + stringFwd + "\nbackwards:\t" + stringBwd + "\n-----------------------------------------------------------------------------\n\n");
+            }
+        }
+    }
+}
