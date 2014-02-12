e6df68804b88fd6ffe0a2cff7b7abb90fcd10251
deinonichus <ds.lutz@gmail.com>
2014-02-12 19:38:25 +0100

    public string

--- ReadWriteFile/ReadWriteFile/Program.cs
+++ ReadWriteFile/ReadWriteFile/Program.cs
@@ -7,12 +7,20 @@ using System.IO;
 
 namespace ReadWriteFile {
     class Program {
+        static string pathSeparator = Path.PathSeparator.ToString();
         static void Main(string[] args) {
+            //public static string pathSeparator = Path.PathSeparator.ToString();
+
             string path = getPath();
             string filename = getFilename();
             // verify that the dir separator is set at the end of path.
             string file = path + filename;
-            string fileContent = getFileContent(file);
+            string fileContent;
+            fileContent = getFileContent(file);
+            // file content doesn't return a string yet. It returns the address of the object file (or something like this). 
+            //string fileLength = getFileLength(fileContent);
+            //string wordCount = getWordCount(fileContent);
+            //string phraseCount = getPhraseCount(fileContent);
 
         }
 
@@ -22,32 +30,45 @@ namespace ReadWriteFile {
             try {
                 fileContent = File.ReadAllText(file);
             }
+            catch (FileNotFoundException) {
+                Console.WriteLine("The file " + file + " was not found.");
+            }
             catch (IOException ex) {
                 Console.WriteLine("The following Error occured:\n" + ex);
             }
             Console.WriteLine("\n\n" + fileContent + "\n\n");
+            Console.ReadKey();
             return fileContent;
         }
 
         // get file length
         static int getFileLength(string fileContent) {
             int fileLength = 0;
-
+            fileLength = fileContent.Length;
             return fileLength;
         }
 
         // get word count
+        static int getWordCount(string fileContent) {
+            int wordCount = 0;
+
+            return wordCount;
+        }
 
         // get phrase count
+        static int getPhraseCount(string fileContent) {
+            int phraseCount = 0;
 
+            return phraseCount;
+        }
 
         // get Path
         static string getPath() {
             Console.WriteLine("Please specify the directory you want to work with.");
-            Console.WriteLine("Leave empty to keep the default Path \"%homepath%/documents/\".");
+            Console.WriteLine("Leave empty to keep the default Path \"%homepath%" + Path.PathSeparator + "documents" + Path.PathSeparator + "\"");
             string path = Console.ReadLine();
             if (path.Equals("")) {
-                path = "%homepath%documents";
+                path = "%homepath%" + pathSeparator + "documents" + pathSeparator;
             }
             return path;
         }
