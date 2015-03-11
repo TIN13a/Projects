668327879fbf2de70716d94fec23d44fb4c2ae90
Stranderer <ajoerg@projekt-u.ch>
2015-03-11 15:45:52 +0100

    Initialisation PartsController

---
+++ AJDSL/Controller/PartsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJDSL {
+    class PartsController {
+        private List<Part> _parts = new List<Part>();
+
+        /// <summary>
+        /// Loads existing Data and send it to form
+        /// </summary>
+        public void loadForm() {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>bool true if added, false if not</returns>
+        public bool addPart() {
+
+            //Load Form Data
+
+            return true;
+        }
+
+        public bool editPart() {
+            return true;
+        }
+    }
+}
