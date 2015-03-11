d0688989a43c5b3912738c7802a2266225de0e3a
deinonichus <ds.lutz@gmail.com>
2015-03-11 15:19:34 +0100

    model with function to read partnumbers from DB.

--- AJDSL/Form1.cs
+++ AJDSL/Form1.cs
@@ -12,7 +12,9 @@ namespace AJDSL {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-            //
+            PartsModel myModel = new PartsModel("(local)", "Parts");
+            MessageBox.Show(myModel.printPartNumbers());
+
         }
     }
 }
