e09ffe2a21aa24ed608b9612aaa9caa8e4370781
Stranderer <ajoerg@projekt-u.ch>
2015-03-17 20:22:33 +0100

    Part cleanup, added comments

--- AJDSL/Model/Part.cs
+++ AJDSL/Model/Part.cs
@@ -17,12 +17,17 @@ namespace AJDSL {
         float height;
         string description;
 
+        /// <summary>
+        /// Construct a new part object.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="partNr"></param>
         public Part(int id, string partNr) {
             this.id = id;
             this.PartNumber = partNr;
         }
 
-
+        //Geter and Setter
         public int Id { 
             get { return this.id; } 
             set { this.id = value; } 
@@ -64,33 +69,40 @@ namespace AJDSL {
             set { this.description = value; }
         }
 
-        public List<Part> getParents() {
-            return parents;
-        }
-
-        public List<Part> getChilds() {
-            return childs;
-        }
-
+        /// <summary>
+        /// Add part as parent.
+        /// </summary>
+        /// <param name="part"></param>
         public void addParent(Part part) {
             if (parents.IndexOf(part) == -1) {
                 parents.Add(part);
             }
         }
 
+        /// <summary>
+        /// Add part as child.
+        /// </summary>
+        /// <param name="part"></param>
         public void addChild(Part part) {
             if (childs.IndexOf(part) == -1) {
                 childs.Add(part);
-                System.Console.WriteLine(id.ToString());
             }
         }
 
+        /// <summary>
+        /// Remove part from parents.
+        /// </summary>
+        /// <param name="part"></param>
         public void removeParent(Part part) {
             if (parents.IndexOf(part) != -1) {
                 parents.Remove(part);
             }
         }
 
+        /// <summary>
+        /// Remove part from childs.
+        /// </summary>
+        /// <param name="part"></param>
         public void removeChild(Part part) {
             if (childs.IndexOf(part) != -1) {
                 childs.Remove(part);
